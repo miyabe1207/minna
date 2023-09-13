e9c37380e3a252596f149ee188b93cc3f2da5438
miyabe1207 <1921339a@hgu.jp>
2023-09-13 23:16:52 +0900

    minna投稿詳細表示

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+}
