a24a0a9bc90f2c52f22ea8b0d8d28a7b95acd5b3
miyabe1207 <1921339a@hgu.jp>
2023-09-19 10:40:30 +0900

    削除機能

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+    
+    protected $fillable = [
+        'disease_name',
+        'therapeutic_method',
+        'hospital_name',
+        'doctor_name',
+    ];
+    
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+}
